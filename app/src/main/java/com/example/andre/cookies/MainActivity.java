2c5eabc0a4783b75bbb7313b037c47e9badeef58
andreluiskg <andreluiskg@hotmail.com>
2017-07-11 18:17:59 -0300

    Initial commit

---
+++ app/src/main/java/com/example/andre/cookies/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.andre.cookies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean hungry = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the cookie should be eaten.
+     */
+    public void eatCookie(View view) {
+
+        // TODO: Find a reference to the ImageView in the layout. Change the image.
+        ImageView imageView = (ImageView) findViewById(R.id.android_cookie_image_view);
+        // TODO: Find a reference to the TextView in the layout. Change the text.
+        TextView textView = (TextView) findViewById(R.id.status_text_view);
+
+        if (hungry) {
+            imageView.setImageResource(R.drawable.after_cookie);
+            textView.setText("I'm so full");
+        } else {
+            imageView.setImageResource(R.drawable.before_cookie);
+            textView.setText("I'm so hungry");
+        }
+        ;
+
+        hungry = !hungry;
+
+    }
+}
